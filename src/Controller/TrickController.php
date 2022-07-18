87ccc107756cb4e96845eb64e9fc5e04be175581
Daddypro21 <sndemapro@gmail.com>
2022-07-18 15:01:40 +0100

    Modification du TrickController ,ajout des nouvbelles methodes

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Trick;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TrickController extends AbstractController
 {
@@ -14,5 +16,67 @@ class TrickController extends AbstractController
         return $this->render('trick/index.html.twig', [
             'controller_name' => 'TrickController',
         ]);
+    }
+
+    #[Route('/tricks/create', name: 'app_tricks_create',methods :['GET','POST'])]
+    public function create(Request $request,UserInterface $user,EntityManagerInterface $em):Response 
+    {
+        $trick = new Trick;
+        $form = $this->createForm(TrickType::class,$trick);
+           
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $trick->setUser($user);
+            $em->persist($trick);
+            $em->flush();
+
+            $this->addFlash('success','trick successfully created');
+
+            return $this->redirectToRoute('app_home');
+
+        }
+        return $this->render('trick/create.html.twig',
+        ['formulaire'=>$form->createView()]); 
+    }
+
+    #[Route('/tricks/{id<[0-9]+>}/edit', name: 'app_tricks_edit',methods:["GET","PUT","POST"])]
+    public function edit(Trick $trick,Request $request,EntityManagerInterface $em):Response 
+    {
+        $form = $this->createForm(TrickType::class,$trick);
+    
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $em->flush();
+
+            $this->addFlash('success','trick successfully updated');
+
+            return $this->redirectToRoute('app_home');
+
+        }
+        return $this->render('trick/edit.html.twig',[
+            'formulaire'=>$form->createView(),
+            'trick'=>$trick
+        ]);
+    }
+
+    #[Route('/tricks/{id<[0-9]+>}/delete', name: 'app_tricks_delete',methods :["GET","POST"])]
+    public function delete(Trick $trick,Request $request ,EntityManagerInterface $em):Response 
+    {
+        if($this->isCsrfTokenValid('trick_delete_'.$trick->getId(),$request->request->get('csrf_token'))){
+            $em->remove($trick);
+            $em->flush();
+            $this->addFlash('info','trick succesfully deleted');
+        }
+        return $this->redirectToRoute('app_home');
+    }
+
+    #[Route('/tricks/{id<[0-9]+>}', name: 'app_tricks_show',methods :['GET'])]
+    public function show(Trick $trick,TrickRepository $repo): Response 
+    {
+        //$trick = $repo->find($id);
+        if(! $trick){
+            throw $this->createNotFoundException("Ce trick n'existe pas ");
+        }
+        return $this->render("trick/show.html.twig",compact('trick'));
     }
 }
