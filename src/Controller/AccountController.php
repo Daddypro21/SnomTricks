11f80dfb60846a680b50fdac45bec367a8d0ab6d
Daddypro21 <sndemapro@gmail.com>
2022-08-01 14:38:15 +0100

    ajout de la methode edit

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\UserFormType;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AccountController extends AbstractController
 {
@@ -13,6 +17,26 @@ class AccountController extends AbstractController
     {
         return $this->render('account/index.html.twig', [
             'controller_name' => 'AccountController',
+        ]);
+    }
+
+    #[Route('/account/edit', name: 'app_account_edit', methods:['POST','GET'])]
+    public function edit(Request $request, EntityManagerInterface $em ):Response 
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(UserFormType::class,$user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em->flush();
+
+            $this->addFlash('success','Compte modifié avec succes');
+
+            return $this->redirectToRoute('app_account');
+        }
+        return $this->render('account/edit.html.twig', [
+            'formulaire' => $form->createView()
         ]);
     }
 }
