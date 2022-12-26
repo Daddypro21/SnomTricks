41b47382ada436b3b379063422345cdcce2aceab
Slime <sndemapro@gmail.com>
2022-12-26 18:37:45 +0100

    Ajout d'une nouvelle commande dans le readme et suppression des repository image,video,et updateImage

--- src/Form/UserFormType.php
+++ src/Form/UserFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class UserFormType extends AbstractType
 {
@@ -16,7 +17,7 @@ class UserFormType extends AbstractType
             
             ->add('firstName',TextType::class,['attr' => ['class' => 'bg-light form-text']])
             ->add('lastName',TextType::class,['attr' => ['class' => 'bg-light form-text']])
-            ->add('email',TextType::class,['attr' => ['class' => 'bg-light form-text']])
+            ->add('email',EmailType::class,['attr' => ['class' => 'bg-light form-text']])
             // ->add('roles')
             // ->add('password')
             // ->add('createdAt')
